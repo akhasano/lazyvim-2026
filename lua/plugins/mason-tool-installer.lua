869e09ca919edbd437736aa7508d25d62d2f60a9
Aleksey Khasanov <aleksey.khasanov@x5.ru>
2026-07-11 14:56:19 +0300

    Mason

---
+++ lua/plugins/mason-tool-installer.lua
@@ -0,0 +1,22 @@
+return {
+  {
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    dependencies = { "mason-org/mason.nvim" },
+    config = function()
+      require("mason-tool-installer").setup({
+        ensure_installed = {
+          "prettier",
+          "markdownlint-cli2",
+          "gofumpt",
+          "goimports",
+          "shfmt",
+          "stylua",
+        },
+        -- Автоматически обновлять инструменты при старте (по желанию, false чтобы не спамить)
+        auto_update = false,
+        -- Запускать установку сразу после старта Neovim
+        run_on_start = true,
+      })
+    end,
+  },
+}
