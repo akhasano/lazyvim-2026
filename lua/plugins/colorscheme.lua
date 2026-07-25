183e08e93e14307b21f4ca1b9470f761f6d28c1c
Aleksey Khasanov <aleksey.khasanov@x5.ru>
2026-07-25 12:39:22 +0300

    colorschemas

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,4 +1,23 @@
-return {}
+return {
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "tokyonight-night",
+    },
+  },
+  -- config = function()
+  --   vim.cmd("colorscheme tokyonight-night")
+  -- end,
+}
+
+-- return {
+--   {
+--     "rebelot/kanagawa.nvim",
+--     config = function()
+--       vim.cmd("colorscheme kanagawa")
+--     end,
+--   },
+-- }
 -- return {
 --   {
 --     "marko-cerovac/material.nvim",
@@ -6,4 +25,20 @@ return {}
 --       vim.cmd("colorscheme material-darker")
 --     end,
 --   },
+-- }
+--
+-- lua/plugins/rose-pine.lua
+-- return {
+--   "rose-pine/neovim",
+--   name = "rose-pine",
+--   config = function()
+--     vim.cmd("colorscheme rose-pine")
+--   end,
+-- }
+
+-- return {
+--   "datsfilipe/min-theme.nvim",
+--   config = function()
+--     vim.cmd("colorscheme min-theme")
+--   end,
 -- }
